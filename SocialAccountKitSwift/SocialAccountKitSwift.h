85eff141b0b90f28a7c576a8b771f3afa3332a9a
MagickWorX <kouichi.abe.wall@magickworx.com>
2017-09-15 13:19:12 +0900

    Initial Commit

---
+++ SocialAccountKitSwift/SocialAccountKitSwift.h
@@ -0,0 +1,19 @@
+//
+//  SocialAccountKitSwift.h
+//  SocialAccountKitSwift
+//
+//  Created by Kouichi ABE (WALL) on 2017/09/15.
+//  Copyright © 2017年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocialAccountKitSwift.
+FOUNDATION_EXPORT double SocialAccountKitSwiftVersionNumber;
+
+//! Project version string for SocialAccountKitSwift.
+FOUNDATION_EXPORT const unsigned char SocialAccountKitSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialAccountKitSwift/PublicHeader.h>
+
+
